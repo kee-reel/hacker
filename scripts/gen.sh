5cd4b7ddee438bfee59c684bb68d05293b85ed15
kee-reel <c4rb0n_unit@protonmail.com>
2022-10-02 18:30:34 +0300

    Path independent gen

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,2 +1,13 @@
 ./scripts/update_status.sh
-sudo hugo
+hugo
+mv generated ..
+git stash
+git checkout gh-pages
+rm -rf ./*
+mv ../generated ./
+mv ./generated/* .
+rmdir ./generated
+git add .
+git commit --amend
+git checkout master
+git stash pop
